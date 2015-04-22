1d28d2426b9ba0d8c18ae54f730785a000fda30e
J DINESH REDDY <dinesh.reddy5225@gmail.com>
2015-04-22 12:59:35 +0530

    removed unnecessory migration files

--- db/migrate/20141009082350_add_password_hash_to_customers.rb
+++
@@ -1,5 +0,0 @@
-class AddPasswordHashToCustomers < ActiveRecord::Migration
-  def change
-    add_column :customers, :password_hash, :string
-  end
-end
--- db/migrate/20141016115445_remove_password_conformation_from_customers.rb
+++
@@ -1,5 +0,0 @@
-class RemovePasswordConformationFromCustomers < ActiveRecord::Migration
-  def change
-    remove_column :customers, :password_conformation, :string
-  end
-end
